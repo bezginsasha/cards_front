b8a36dfd7ad8264033309e0daa9f8f7d3558be0c
bezgin sasha <bezgin.sasha06@gmail.com>
2021-07-07 23:35:35 +0700

    add state value is Search

--- src/Search.js
+++ src/Search.js
@@ -2,9 +2,23 @@ import React from 'react';
 import './Search.css';
 
 class Search extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: ''
+		};
+	}
+
+	inputHandler = (event) => {
+		event.preventDefault();
+		this.setState({
+			value: event.target.value
+		});
+	}
+
 	render() {
 		return (
-			<input type="text" className="search" placeholder="Search"/>
+			<input type="text" className="search" placeholder="Search" value={ this.state.value } onInput={ this.inputHandler } />
 		);
 	}
 }
