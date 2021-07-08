0f1c0cf86464ad2fca567a9cd11219c946d47743
bezgin sasha <bezgin.sasha06@gmail.com>
2021-07-08 20:03:47 +0700

    add const FORMS - dictionary containing all forms in string

--- src/App.js
+++ src/App.js
@@ -11,6 +11,16 @@ import AccountForm from './forms/AccountForm';
 import GameForm from './forms/GameForm';
 import HelpForm from './forms/HelpForm';
 
+export const FORMS = {
+	newWord: 'New word',
+	game: 'Game',
+	piles: 'Piles',
+	help: 'Help',
+	import: 'Import',
+	account: 'Account',
+	none: 'None',
+};
+
 class App extends React.Component {
 	constructor(props) {
 		super(props);
