7d746080dddd16c2e816f9e2345d00969ac2ad0f
bezgin sasha <bezgin.sasha06@gmail.com>
2021-07-08 19:17:32 +0700

    move index js content to app component

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './index.css';
+
+import Search from './Search';
+import HeaderButton from './HeaderButton';
+import WordListItem from './WordListItem';
+import OverContainer from './OverContainer';
+
+import NewWordForm from './forms/NewWordForm';
+import AccountForm from './forms/AccountForm';
+import GameForm from './forms/GameForm';
+import HelpForm from './forms/HelpForm';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return(
+			<React.Fragment>
+				<section className="header" >
+					<HeaderButton buttonValue="New word" />
+					<HeaderButton buttonValue="Game" />
+					<HeaderButton buttonValue="Piles" />
+					<HeaderButton buttonValue="Help" />
+					<HeaderButton buttonValue="Import" />
+					<HeaderButton buttonValue="Account" />
+				</section>
+				<hr />
+				<Search />
+				<WordListItem wordValue="Word" />
+				<WordListItem wordValue="School" />
+				<WordListItem wordValue="Magic" />
+
+				<OverContainer>
+					<HelpForm />
+				</OverContainer>
+			</React.Fragment>
+		)
+	}
+}
+
+export default App
