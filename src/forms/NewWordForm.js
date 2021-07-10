0d5fb7eb1e9c9b226869a0d236054708b51272a5
bezgin sasha <bezgin.sasha06@gmail.com>
2021-07-11 00:01:01 +0700

    add input listners on new word form

--- src/forms/NewWordForm.js
+++ src/forms/NewWordForm.js
@@ -5,14 +5,44 @@ import OverForm from './OverForm'
 class NewWordForm extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+		    originalWord: '',
+		    translatedWord: ''
+		}
 	}
+	}
+
+	originalWordInputHandler = (event) => {
+	    this.setState({
+	        originalWord: event.target.value
+	    })
+	};
+
+	translatedWordInputHandler = (event) => {
+	    this.setState({
+	        tranlatedValue: event.target.value
+	    })
+	};
 
 	render() {
 		return (
 			<OverForm>
 				<p>New word</p>
-				<textarea className="over-input-textarea" placeholder="Original word" rows="3"></textarea>
-				<textarea className="over-input-textarea" placeholder="Translated word with context" rows="3"></textarea>
+				<textarea
+				    onInput={ this.originalWordInputHandler }
+				    value={ this.state.originalWord }
+				    className="over-input-textarea"
+				    placeholder="Original word"
+				    rows="3"
+				/>
+                <br/>
+				<textarea
+				    onInput={ this.translatedWordInputHandler }
+				    value={ this.state.tranlatedValue }
+                    className="over-input-textarea"
+                    placeholder="Translated word with context"
+				    rows="3"
+				/>
 				<br />
 				<input type="button" value="Save" className="over-form-button" />
 			</OverForm>
