96c2b084b31e698accbc48e6fd622703214bbea4
bezgin sasha <bezgin.sasha06@gmail.com>
2021-07-24 15:26:05 +0700

    refactor: write OverForm component in several lines

--- src/forms/OverForm.js
+++ src/forms/OverForm.js
@@ -3,7 +3,12 @@ import React from 'react';
 
 function OverForm(props) {
     return (
-        <div className="over-form" onMouseDown={ (event) => event.stopPropagation() }>{ props.children }</div>
+        <div
+            className="over-form"
+            onMouseDown={ (event) => event.stopPropagation() }
+        >
+            { props.children }
+        </div>
     );
 }
 
