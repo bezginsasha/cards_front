23cdfbf25c7428847621f0216ca068ea247308b4
bezgin sasha <bezgin.sasha06@gmail.com>
2021-07-26 21:37:50 +0700

    add more button for words in list

--- src/WordListItem.js
+++ src/WordListItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './WordListItem.css';
+import more from './more.svg';
 
 function WordListItem(props) {
 	return (
-		<p className="word-list-item">{ props.value }</p>
+		<p className="word-list-item">
+			{ props.value }
+			<img src={ more } className="more-button" />
+		</p>
 	);
 }
 
