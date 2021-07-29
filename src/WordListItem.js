fd562f15970391f8b476e56469de7c1c303bd95c
bezgin sasha <bezgin.sasha06@gmail.com>
2021-07-29 19:36:25 +0700

    refactor

--- src/WordListItem.js
+++ src/WordListItem.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './WordListItem.css';
 import more from './more.svg';
 import ContextMenu from './ContextMenu'
