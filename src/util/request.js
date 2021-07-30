8a9efcff36c2d7d1aa4ecbf07d3d997219df4878
bezgin sasha <bezgin.sasha06@gmail.com>
2021-07-30 22:12:42 +0700

    request function - add default GET method

--- src/util/request.js
+++ src/util/request.js
@@ -1,12 +1,12 @@
 // argument of this function - object params that have options:
 //   - url - part of url that follows after root url of site
-//   - method - POST, GET ...
+//   - method - default: GET
 //   - body - that passes to POST request
 //   - callback - function that would be called after successful request
 function request(params) {
 	var rootUrl = 'http://localhost:5000/';
 	var targetUrl = rootUrl + params.url;
-	var method = params.method;
+	var method = params.method || 'GET';
 	var body = params.body;
 	var callback = params.callback;
 
