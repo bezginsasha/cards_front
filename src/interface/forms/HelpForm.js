b8fb965216da9f3fc37735d8ce79644fe56f5f2e
bezgin sasha <bezgin.sasha06@gmail.com>
2021-07-31 12:35:25 +0700

    refactor: HelpForm from class to function based

--- src/interface/forms/HelpForm.js
+++ src/interface/forms/HelpForm.js
@@ -2,19 +2,13 @@ import React from 'react';
 import './CommonForm.css';
 import OverForm from './OverForm'
 
-class HelpForm extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return (
-			<OverForm>
-				<p>Help</p>
-				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias non expedita, provident veniam modi voluptas enim beatae, magni accusantium libero dolorem aperiam maxime asperiores fugit obcaecati quaerat, ipsa? Vero, mollitia repudiandae architecto, excepturi officia sint quod ipsam eum nulla voluptatem esse modi laborum libero? Architecto cupiditate blanditiis reiciendis, vel unde id recusandae, incidunt necessitatibus harum dicta atque mollitia fugiat distinctio, nobis illum fugit quisquam consequuntur ex? Aut, perferendis non iure eveniet delectus vel praesentium minima sint neque quo officiis eaque nostrum quis aliquid eius sed amet optio consequuntur adipisci eligendi voluptates natus. Quisquam repudiandae dicta deserunt, illum assumenda aperiam laudantium.</p>
-			</OverForm>
-		);
-	}
+function HelpForm() {
+	return (
+		<OverForm>
+			<p>Help</p>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias non expedita, provident veniam modi voluptas enim beatae, magni accusantium libero dolorem aperiam maxime asperiores fugit obcaecati quaerat, ipsa? Vero, mollitia repudiandae architecto, excepturi officia sint quod ipsam eum nulla voluptatem esse modi laborum libero? Architecto cupiditate blanditiis reiciendis, vel unde id recusandae, incidunt necessitatibus harum dicta atque mollitia fugiat distinctio, nobis illum fugit quisquam consequuntur ex? Aut, perferendis non iure eveniet delectus vel praesentium minima sint neque quo officiis eaque nostrum quis aliquid eius sed amet optio consequuntur adipisci eligendi voluptates natus. Quisquam repudiandae dicta deserunt, illum assumenda aperiam laudantium.</p>
+		</OverForm>
+	);
 }
 
 export default HelpForm;
