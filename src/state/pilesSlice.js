f5745cce7c395130369f703f608963f23f849c62
bezginsasha <bezgin.sasha06@gmail.com>
2021-08-10 19:10:29 +0700

    add initial and separated update reducers in slice

--- src/state/pilesSlice.js
+++ src/state/pilesSlice.js
@@ -1,37 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-var initialState = [
-	'word',
-	'test',
-	'hello',
-];
+var initialState = [];
 
 var pilesSlice = createSlice({
 	name: 'piles',
 	initialState,
 	reducers: {
+		initiatePiles: (state, action) => {
+			state.push(...action.payload);
+		},
 		deletePile: (state, action) => {
 			var index = state.indexOf(action.payload);
 			state.splice(index, 1);
 		},
-		updateOrInsertPile: (state, action) => {
+		insertPile: (state, action) => {
+			state.push(action.payload);
+		},
+		updatePile: (state, action) => {
 			var oldName = action.payload.oldName;
 			var newName = action.payload.newName;
 
-			if (!newName) {
-				return;
-			}
-
-			if (oldName) {
-				var index = state.indexOf(oldName);
-				state[index] = newName;
-			} else {
-				state.push(newName);
-			}
+			var index = state.indexOf(oldName);
+			state[index] = newName;
 		}
 	}
 });
 
-export const { deletePile, updateOrInsertPile } = pilesSlice.actions;
+export const { initiatePiles, deletePile, insertPile, updatePile } = pilesSlice.actions;
 
 export default pilesSlice.reducer
