3c8c92d5334afffcaa6624cb90748e5be9db0c53
bezginsasha <bezgin.sasha06@gmail.com>
2021-08-19 21:16:36 +0700

    setPile by click on pile in App component

--- src/interface/Pile/Pile.js
+++ src/interface/Pile/Pile.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import { useDispatch } from "react-redux";
+import { setPile } from '../../state/currentPileSlice'
 import './Pile.css'
 
 function Pile(props) {
+	var dispatch = useDispatch();
 	var className = 'pile';
 	if (props.currentPile === props.name) {
 		className += ' pile-selected'
 	}
 
+	function pileClickHandler(event) {
+		dispatch(setPile(event.target.value));
+	}
+
 	return (
 		<input
 			type="button"
+			onClick={ pileClickHandler }
 			className={ className }
 			value={ props.name }
 		/>
