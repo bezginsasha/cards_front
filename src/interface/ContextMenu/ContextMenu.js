9f26e2c7ddc60fbe3cae5d388a9ba78229b18539
bezginsasha <bezgin.sasha06@gmail.com>
2021-08-23 19:33:52 +0700

    context menu - add move to pile buttons

--- src/interface/ContextMenu/ContextMenu.js
+++ src/interface/ContextMenu/ContextMenu.js
@@ -1,10 +1,15 @@
 import './ContextMenu.css'
-import { useDispatch } from 'react-redux'
-import { deleteCard } from '../../state/cardsSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import {deleteCard, moveCard} from '../../state/cardsSlice'
 import request from "../../util/request";
 
 function ContextMenu(props) {
 	var dispatch = useDispatch();
+
+	// construction with [... ] needs because piles is readonly
+	var piles = [...useSelector(state => state.piles)];
+	var indexPileOfCard = piles.indexOf(props.card.pileName);
+	piles.splice(indexPileOfCard, 1);
 
 	function deleteCardHandler() {
 		var form = new FormData();
@@ -18,6 +23,23 @@ function ContextMenu(props) {
 				console.log(data);
 				dispatch(deleteCard(props.card.id));
 			}
+		});
+	}
+
+	function moveCardToPile(newPile) {
+		var form = new FormData();
+		form.set('cardId', props.card.id);
+		form.set('pileName', newPile);
+
+		dispatch(moveCard({
+			cardId: props.card.id,
+			pileName: newPile
+		}));
+
+		request({
+			url: 'cards/move',
+			method: 'POST',
+			body: form
 		});
 	}
 
@@ -39,6 +61,17 @@ function ContextMenu(props) {
 				value="delete"
 				className="context-menu-button"
 			/>
+			{
+				piles.map(pile => (
+					<input
+						type="button"
+						onClick={ () => moveCardToPile(pile) }
+						value={ 'to ' + pile }
+						key={ props.card.id + pile }
+						className="context-menu-button"
+					/>
+				))
+			}
 		</div>
 	)
 }
