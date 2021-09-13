76bc2c657d3ed45b1f10fd8ebc3fe9ab17789525
bezginsasha <bezgin.sasha06@gmail.com>
2021-09-13 20:16:52 +0700

    over form - make correct displaying in case over form more then over container

--- src/interface/forms/OverForm.js
+++ src/interface/forms/OverForm.js
@@ -1,7 +1,20 @@
 import './CommonForm.css'
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function OverForm(props) {
+	// This hook need to set height of container in case - text height more than container height
+	// Without it gray and blurred background's height only of monitor size
+	useEffect(() => {
+		var form = document.querySelector('.over-form');
+		var formHeight = form.offsetHeight + 120;
+
+		var container = document.querySelector('.over-container');
+		var containerHeight = container.offsetHeight;
+
+		if (formHeight > containerHeight)
+			container.style.height = formHeight + 'px';
+	});
+
 	return (
 		<div
 			className="over-form"
